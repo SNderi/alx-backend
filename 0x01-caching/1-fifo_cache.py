85d1892bfd324d84dfede5324d00afd33f7cbcaa
Sharon Nderi <nderisharonw@gmail.com>
2023-01-27 16:33:46 +0300

    Corection

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -2,36 +2,32 @@
 """ FIFOCache module that inherits from BaseCaching
 """
 
-try:
-    from base_caching import BaseCaching
+from base_caching import BaseCaching
 
 
-    class FIFOCache(BaseCaching):
-        """A caching system using FIFO- First In First Out.
+class FIFOCache(BaseCaching):
+    """A caching system using FIFO- First In First Out.
+    """
+    def __init__(self):
+        """Initializes cache data.
         """
-        def __init__(self):
-            """Initializes cache data.
-            """
-            super().__init__()
+        BaseCaching.__init__(self)
 
-        def put(self, key, item):
-            """Add key value data to the cache.
-            """
-            if key is None or item is None:
-                return
+    def put(self, key, item):
+        """Add key value data to the cache.
+        """
+        if key is None or item is None:
+            return
 
-            self.cache_data[key] = item
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                del_key = list(self.cache_data.keys())[0]
-                print("Discard: {}".format(del_key))
-                del self.cache_data[del_key]
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            del_key = list(self.cache_data.keys())[0]
+            print("Discard: {}".format(del_key))
+            del self.cache_data[del_key]
 
-        def get(self, key):
-            """ Retrieves key's value.
-            """
-            if key is None or key not in self.cache_data.keys():
-                return None
-            return self.cache_data[key]
-
-except:
-    print(sys.exc_info()[1])
+    def get(self, key):
+        """ Retrieves key's value.
+        """
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data[key]
